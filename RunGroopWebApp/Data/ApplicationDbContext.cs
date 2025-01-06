718f748260f17f44eb77c646f96ae46cdaf0d936
Aniket panchal <aniketpanchal07@gmail.com>
2025-01-06 22:05:55 +0530

    done with the models , dbcontext and migrations etc...

---
+++ RunGroopWebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RunGroopWebApp.Models;
+
+namespace RunGroopWebApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Race> Races { get; set; }
+        public DbSet<Club> Clubs { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<State> States { get; set; }
+        public DbSet<City> Cities { get; set; }
+    }
+}
---
+++ RunGroopWebApp/Models/AppUser.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace RunGroopWebApp.Models
+{
+    public class AppUser : IdentityUser
+    {
+        public int? Pace { get; set; }
+        public int? Mileage { get; set; }
+        public string? ProfileImageUrl { get; set; }
+        public string? City { get; set; }
+        public string? State { get; set; }
+        [ForeignKey("Address")]
+        public int? AddressId { get; set; }
+        public Address? Address { get; set; }
+    }
+}
